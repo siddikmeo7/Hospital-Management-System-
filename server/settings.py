0d234382e28f787e02466ba2e9a249495c4ff6b2
abubakr <siddikme.o7@gmail.com>
2024-11-30 12:19:35 +0500

    adding own statics where will be stored static files!

--- server/settings.py
+++ server/settings.py
@@ -101,8 +101,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Directory for collected static files in production
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # This is where you will store your CSS, JS, and images
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Directory for collected static files in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
